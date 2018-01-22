df6b3a626669f6bd3cb47f24b6a23227c5bfc736
Brooke Lewis <belv2c@gmail.com>
2018-01-22 15:12:15 -0600

    finished assignment

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -24,6 +24,40 @@ namespace PlanetsAndSpaceships
             List<string> rockyPlanetsList = planetList.GetRange(0, 4).ToList();
 
             planetList.Remove("Pluto");
+
+
+            Dictionary<string, string> probeDictionary = new Dictionary<string, string>()
+            {
+                { "Mariner 10", "Mercury" },
+                { "Messenger", "Murcury" },
+                { "Mariner 2", "Venus" },
+                { "Venera 4", "Venus" },
+                { "Mariner 5", "Venus" },
+                { "Mariner 4", "Earth" },
+                { "Mariner 6", "Earth" },
+                { "Mariner 7", "Earth" },
+                { "Pioneer 10", "Jupiter" },
+                { "Pioneer 11", "Jupiter" },
+                { "Voyager 4", "Jupiter" },
+                { "Cassini", "Saturn" },
+                { "Voyager 2", "Neptune" },
+                { "Voyager 1", "Jupiter" }
+
+            };
+
+
+            foreach(var planet in planetList)
+            {
+                Console.Write($"{planet}:");
+                foreach(var probe in probeDictionary)
+                {
+                    if (probe.Value == planet)
+                    {
+                        Console.Write($"{probe.Key}");
+                        Console.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
