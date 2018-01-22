ff04f2814f07e6733172f901b10d611bce7e0fe0
Brooke Lewis <belv2c@gmail.com>
2018-01-22 14:26:55 -0600

    planet list is in order

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PlanetsAndSpaceships
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> planetList = new List<string>()
-            {
-                "Mercury", "Mars"
-            };
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastPlanetList = new List<string>() { "Uranus", "Neptune" };
+            planetList.AddRange(lastPlanetList);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanetsList = planetList.GetRange(0, 4).ToList();
+
+            planetList.Remove("Pluto");
         }
     }
 }
